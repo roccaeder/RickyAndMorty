85b12bcdd183a8ec82a688f5a79f8a13f7424e0d
eder <edra1791@gmail.com>
2021-11-20 13:05:22 -0500

    refactorize the code

--- src/services/episodelocation.js
+++ src/services/episodelocation.js
@@ -1,22 +1,22 @@
 import { FetchUrl } from "./fetchurl";
 
-export async function episodeLocation(infochar) {
+export async function EpisodeLocation(infochar) {
   let arr = [];
   let result = await FetchUrl(`https://rickandmortyapi.com/api/episode`);
-  arr = result.results.reduce((acc, cur) => {
-    let locations = cur.characters.map(char => infochar[char.split("/").pop() - 1].origin.name);
-    locations = [...new Set(locations)];
-    return acc.concat(GenerateHash(cur.name, cur.episode, locations));
-  }, []);
+  arr = JoinArray( result, infochar);
   while(result.info.next) {
     result = await FetchUrl(result.info.next);
-    arr = arr.concat(result.results.reduce((acc, cur) => {
+    arr = arr.concat(JoinArray( result, infochar));
+  }
+  return arr;
+}
+
+function JoinArray( result, infochar){
+  return result.results.reduce((acc, cur) => {
       let locations = cur.characters.map(char => infochar[char.split("/").pop() - 1].origin.name);
       locations = [...new Set(locations)];
       return acc.concat(GenerateHash(cur.name, cur.episode, locations));
-    }, []));
-  }
-  return arr;
+    }, []);
 }
 
 function GenerateHash( name, episode, locations ) {
