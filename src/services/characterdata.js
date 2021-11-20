fda04f0cfc5b799c2bb6e25f0220ac01167f5880
eder <edra1791@gmail.com>
2021-11-20 12:50:05 -0500

    get all data of characters

---
+++ src/services/characterdata.js
@@ -0,0 +1,13 @@
+import { FetchUrl } from "./fetchurl";
+
+export async function CharactersData() {
+  let arr = [];
+  let result = await FetchUrl(`https://rickandmortyapi.com/api/character`)
+  arr = result.results;
+  while(result.info.next) {
+    result = await FetchUrl(result.info.next);
+    arr = arr.concat(result.results);
+  }
+  // sessionStorage.setItem("characters", JSON.stringify(arr));
+  return arr;
+}
